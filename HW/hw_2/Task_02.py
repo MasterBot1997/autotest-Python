79054e668058d4226e36baee1d4afef9f271fc7c
Ivan Belyaev <bel3187228@yandex.ru>
2023-09-30 19:17:26 +0300

    Завершена вторая задача
    Реализована с функцией проверки остатка от деления

--- HW/hw_2/Task_02.py
+++ HW/hw_2/Task_02.py
@@ -1,12 +1,32 @@
-# Напишите программу, которая получает целое число и возвращает 
-# его шестнадцатеричное строковое представление. 
+# Напишите программу, которая получает целое число и возвращает
+# его шестнадцатеричное строковое представление.
 # Функцию hex используйте для проверки своего результата.
 
-number = int(input('Введите число: '))
-print(bin(number))
+# Функция проверки остатка для замены на соответствующее буквенное значение
+def checking_the_remainder_of_the_division(num):
+    if num == 10:
+        return "a"
+    elif num == 11:
+        return "b"
+    elif num == 12:
+        return "c"
+    elif num == 13:
+        return "d"
+    elif num == 14:
+        return "e"
+    elif num == 15:
+        return "f"
+    else:
+        return num
+
+# Константа делителя для избежания магического числа
+DEl = 16
+number = int(input("Введите число: "))
+res = ''
+print(hex(number))
 
 while number:
-    res = f'{number%i}' + res
-    number //= i
-    
-print(i, res, sep= ' - ')
+    res = f"{checking_the_remainder_of_the_division(number%DEl)}" + res
+    number //= DEl
+
+print(res)
