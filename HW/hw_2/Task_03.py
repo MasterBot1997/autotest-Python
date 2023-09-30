f8df7238f0b21a2ab69e00f926bac9ea954d1ceb
Ivan Belyaev <bel3187228@yandex.ru>
2023-09-30 19:39:44 +0300

    Выполнено третье задание
    Реализовано через 2 функции:
     - Сложение дробей
     - Умножение дробей
    
    Обе функции возвращают отформатированное строковое значение для вывода в консоль

--- HW/hw_2/Task_03.py
+++ HW/hw_2/Task_03.py
@@ -1,3 +1,21 @@
-# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем. 
-# Программа должна возвращать сумму и произведение* дробей. 
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей.
 # Для проверки своего кода используйте модуль fractions.
+
+fraction_1 = input("Введите первую дробь в виде a/b: ")
+fraction_2 = input("Введите вторую дробь в виде a/b: ")
+numerato_1, denominator_1 = map(int, fraction_1.split("/"))
+numerato_2, denominator_2 = map(int, fraction_2.split("/"))
+
+
+def multiplying_fractions(a,b,c,d):
+    return f'{a * c}/{b * d}'
+
+def sum_of_fractions(a,b,c,d):
+    new_a = a * d
+    new_c = c * b
+    new_denominator = d * b
+    return f'{new_a + new_c}/{new_denominator}'
+
+print(f'{fraction_1}', ' + ', f'{fraction_2}', ' = ', sum_of_fractions(numerato_1, denominator_1, numerato_2, denominator_2))
+print(f'{fraction_1}', ' * ', f'{fraction_2}', ' = ', multiplying_fractions(numerato_1, denominator_1, numerato_2, denominator_2))
